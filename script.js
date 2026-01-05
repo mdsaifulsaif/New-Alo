be7b986b584d6f78561e716254a59cc66a52da23
Md Saiful Saif <mdsaifulislam02137@gmail.com>
2026-01-05 16:55:40 +0600

    Add news data and enhance header with sticky navbar and icons

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// alert("hello wrold");
+const navbar = document.getElementById("navbar");
+const topBar = document.getElementById("topBar");
+
+let lastScrollY = window.scrollY;
+const stickyPoint = navbar.offsetTop;
+
+window.addEventListener("scroll", () => {
+  const currentScrollY = window.scrollY;
+
+  // Navbar sticky logic
+  if (currentScrollY >= stickyPoint) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  // fixd logic
+  if (currentScrollY > lastScrollY) {
+    // scrolling down
+    topBar.classList.add("hide");
+  } else {
+    // scrolling up
+    topBar.classList.remove("hide");
+  }
+
+  lastScrollY = currentScrollY;
+});
+
+console.log(newsData);
